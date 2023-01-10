01b75206af70e0f9bec6c0cecf94aefb6d86438d
Michael Kolinski <michaelkolinski150@gmail.com>
2023-01-10 13:15:33 +0400

    tuesday 10 junary

--- lib/widgets/SellersListWidget.dart
+++ lib/widgets/SellersListWidget.dart
@@ -1,1 +1,26 @@
-// class SellersListWidget { }
+import 'package:flutter/material.dart';
+import '../models/sellerModel.dart';
+
+// class Todo {
+//   final String title;
+//   final String description;
+
+//   Todo(this.title, this.description);
+// }
+
+class SellersListWidget extends StatelessWidget {
+  final List<SellerModel> sellersList;
+
+  SellersListWidget(this.sellersList);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: this.sellersList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Text('ID ${this.sellersList[index].sellerInfo?.email}',
+              style: TextStyle(fontSize: 22));
+        });
+  }
+}
